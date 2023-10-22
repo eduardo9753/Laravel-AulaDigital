1ae7fbcec582d9dfaed9e5073d6d164ef8ff3a52
Eduardo <picis_9716@hotmail.com>
2023-10-22 10:05:33 -0500

    PLataforma AulaDigital v.1.0

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,10 +88,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
-    <!-- CDN JQUERY -->
-    <script src="https://code.jquery.com/jquery-2.2.4.min.js"
-        integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-
 
     {{-- links js mapa leaflet --}}
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
