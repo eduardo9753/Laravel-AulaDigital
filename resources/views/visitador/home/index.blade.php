7dc9584415afe35fea460f1c2177c7decf92b934
Eduardo <picis_9716@hotmail.com>
2023-11-23 15:47:59 -0500

    Plataforma AulaDigital v.1.0

--- resources/views/visitador/home/index.blade.php
+++ resources/views/visitador/home/index.blade.php
@@ -73,7 +73,7 @@
                     <div class="col-md-3 my-2">
                         <div class="mi-card">
                             <div class="mi-card-content">
-                                <h2 class="contenido-bloques-titulo">Cotinuidad!</h2>
+                                <h2 class="contenido-bloques-titulo">Continuidad!</h2>
                                 <div class="text-center">
                                     <img style="width: 100px;height: 100px;"
                                         src="https://cdn-icons-png.flaticon.com/512/11421/11421424.png" alt="">
