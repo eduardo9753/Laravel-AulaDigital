429a177001227f7a230bc4a6ef6fca6cfb31eb98
Eduardo <picis_9716@hotmail.com>
2024-03-22 15:06:05 -0500

    PLATAFORMA AulaDigital v.1.5

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -8,73 +8,27 @@
 
                 <h1 class="color-general curso-status-title">{{ $current->name }}</h1>
 
-                {{-- MARCAR COMO CULMINADA LA LECCION --}}
-                <div class="d-flex align-items-center cursor" wire:click="completed">
-                    @if ($current->completed)
-                        <i class='bx bxs-toggle-right' style='color:rgb(52, 152, 219);  font-size: 28px'></i>
-                        <p class="cursor-status" style='font-size: 18px'>lección culminada</p>
-                    @else
-                        <i class='bx bx-toggle-left' style="font-size: 28px"></i>
-                        <p class="cursor-status" style='font-size: 18px'>culminar lección</p>
-                    @endif
-                </div>
-                {{-- MARCAR COMO CULMINADA LA LECCION --}}
+
 
 
                 {{-- NAVEGACiON DE LECCIONES --}}
                 <div class="mt-3 d-flex justify-content-between">
                     @if ($this->previous)
                         <a class="mi-boton general btn-sm" wire:click="changeLesson({{ $this->previous }})"><i
-                                class='bx bx-send bx-flip-horizontal' style="font-size: 40px"></i></a>
+                                class='bx bx-send bx-flip-horizontal' style="font-size: 25px"></i></a>
                     @else
-                        <a class="mi-boton azul btn-sm"><i class='bx bx-trending-up' style="font-size: 40px"></i></a>
+                        <a class="mi-boton azul btn-sm"><i class='bx bx-trending-up' style="font-size: 25px"></i></a>
                     @endif
 
                     @if ($this->next)
                         <a class="mi-boton general btn-sm" wire:click="changeLesson({{ $this->next }})"><i
-                                class='bx bx-send' style="font-size: 40px"></i></a>
+                                class='bx bx-send' style="font-size: 25px"></i></a>
                     @else
                         <a class="mi-boton rojo btn-sm"><i class='bx bx-trending-up bx-flip-horizontal'
-                                style="font-size: 40px"></i></a>
+                                style="font-size: 25px"></i></a>
                     @endif
                 </div>
                 {{-- NAVEGACiON DE LECCIONES --}}
-
-
-                {{-- CITA DEL VIDEO --}}
-                <div class="card">
-                    <div class="card-body">
-                        <?php
-                        $url = $current->url;
-                        $urlParts = parse_url($url);
-                        $channelName = '';
-                        $videoDate = '';
-                        
-                        if (isset($urlParts['query'])) {
-                            parse_str($urlParts['query'], $queryArray);
-                            $channelName = $queryArray['ab_channel'] ?? '';
-                            $videoDate = $queryArray['t'] ?? '';
-                        
-                            if ($videoDate) {
-                                $fecha = 'No disponible'; // Agrega el punto y coma aquí
-                            } else {
-                                $fecha = $videoDate;
-                            }
-                        }
-                        ?>
-                        <div class="alert alert-info alert-dismissible fade show" role="alert">
-                            <strong>Referencia:</strong>
-                            <p>Material extraído de la Web - {{ $current->name }} [Video]. YouTube. Publicado por el
-                                canal
-                                <strong>{{ $channelName }}</strong>. Disponible en: <a target="_blank"
-                                    href="{{ $current->url }}" title="{{ $channelName }}">{{ $url }}</a>
-                            </p>
-                            <button type="button" class="btn-close" data-bs-dismiss="alert"
-                                aria-label="Close"></button>
-                        </div>
-                    </div>
-                </div>
-                {{-- CITA DEL VIDEO --}}
 
 
                 {{-- DESCRIPCION DE LA LECCION --}}
@@ -82,7 +36,7 @@
                     <div class="card">
                         <div class="card-body">
                             <div class="alert alert-info alert-dismissible fade show" role="alert">
-                                <strong>Referencia:</strong>
+                                <strong>Referencia del Material:</strong>
                                 <p>
                                     Universidad Nacional Federico Villarreal. (2018). {{ $course->title }}.
                                     <strong>Recuperado de:</strong>
@@ -117,7 +71,7 @@
                         <h1 class="lead">{{ $course->title }}</h1>
 
                         {{-- DESCRICCION DEL COLABORADOR "profesor" --}}
-                        <div class="d-flex align-items-center">
+                        <div class="d-flex align-items-center mb-3">
                             <figure>
                                 <img src="{{ $course->teacher->profile_photo_url }}" alt="">
                             </figure>
@@ -136,7 +90,22 @@
 
 
                         {{-- BARRA DE PROGRESO --}}
-                        <p class="mt-3">{{ $this->advance . '%' }} Completado</p>
+                        <div class="d-flex justify-content-between">
+                            <p class="">{{ $this->advance . '%' }} Completado</p>
+
+                            {{-- MARCAR COMO CULMINADA LA LECCION --}}
+                            <div class="d-flex align-items-center cursor" wire:click="completed">
+                                @if ($current->completed)
+                                    <i class='bx bxs-toggle-right'
+                                        style='color:rgb(52, 152, 219);  font-size: 28px'></i>
+                                    <p class="cursor-status" style='font-size: 18px'>lección culminada</p>
+                                @else
+                                    <i class='bx bx-toggle-left' style="font-size: 28px"></i>
+                                    <p class="cursor-status" style='font-size: 18px'>culminar lección</p>
+                                @endif
+                            </div>
+                            {{-- MARCAR COMO CULMINADA LA LECCION --}}
+                        </div>
                         <div class="progress mt-2" style="width: 100% !important;">
                             <div class="progress-bar" role="progressbar" style="width: {{ $this->advance . '%' }}"
                                 aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -198,6 +167,46 @@
                             @endforeach
                         </div>
                         {{-- INPRIMIENDO LAS SECCIONES DE LOS CURSOS --}}
+
+
+
+                        {{-- CITA DEL VIDEO --}}
+                        <div class="card my-3">
+                            <div class="card-body">
+                                @php
+                                    $url = $current->url;
+                                    $urlParts = parse_url($url);
+                                    $channelName = '';
+                                    $videoDate = '';
+
+                                    if (isset($urlParts['query'])) {
+                                        parse_str($urlParts['query'], $queryArray);
+                                        $channelName = $queryArray['ab_channel'] ?? '';
+                                        $videoDate = $queryArray['t'] ?? '';
+
+                                        if ($videoDate) {
+                                            $fecha = 'No disponible'; // Agrega el punto y coma aquí
+                                        } else {
+                                            $fecha = $videoDate;
+                                        }
+                                    }
+                                @endphp
+
+                                <div class="alert alert-info alert-dismissible fade show" role="alert">
+                                    <strong>Referencia del Video:</strong>
+                                    <p>Material extraído de la Web - {{ $current->name }} [Video]. YouTube. Publicado
+                                        por el
+                                        canal
+                                        <strong>{{ $channelName }}</strong>. Disponible en: <a target="_blank"
+                                            href="{{ $current->url }}"
+                                            title="{{ $channelName }}">{{ $url }}</a>
+                                    </p>
+                                    <button type="button" class="btn-close" data-bs-dismiss="alert"
+                                        aria-label="Close"></button>
+                                </div>
+                            </div>
+                        </div>
+                        {{-- CITA DEL VIDEO --}}
 
 
                         @section('scripts')
