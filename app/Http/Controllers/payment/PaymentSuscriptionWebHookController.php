259ec4ab15bd4c2751f84918a6a26d52bcac6d0e
Eduardo <picis_9716@hotmail.com>
2024-07-02 13:31:15 -0500

    PLATAFORMA AulaDigital V.1.0

--- app/Http/Controllers/payment/PaymentSuscriptionWebHookController.php
+++ app/Http/Controllers/payment/PaymentSuscriptionWebHookController.php
@@ -14,25 +14,26 @@ class PaymentSuscriptionWebHookController extends Controller
     //
     public function index(Request $request)
     {
-        // Extraer los datos del payload
-        $payloadArray = $request->all();
+        // Obtener el payload del webhook en formato JSON
         $payloadRaw = $request->getContent();
+
+        // Decodificar el JSON del payload
+        $payloadArray = json_decode($payloadRaw, true);
 
         // Registra los datos en el log para depuración (puedes eliminar esto en producción)
         Log::info('Webhook Received (Array):', $payloadArray);
-        Log::info('Webhook Received (Raw):', ['payload' => $payloadRaw]);
 
         // Verificar si se ha recibido el webhook con los datos esperados
         if (isset($payloadArray['data']['id'])) {
             $preapprovalId = $payloadArray['data']['id'];
 
-            // Aquí debes buscar el usuario relacionado con el preapprovalId en tu base de datos
-            $user = auth()->user()->id;
+            // Verificar si el usuario está autenticado
+            if (auth()->check()) {
+                $user = auth()->user();
 
-            if ($user) {
                 // Crear un nuevo registro de pago
                 $pay = Pay::create([
-                    'user_id' => $user,
+                    'user_id' => $user->id,
                     'collection_id' => '',
                     'collection_status' => 'PLAN-PRE-UNI',
                     'payment_id' => $preapprovalId,
@@ -52,16 +53,16 @@ class PaymentSuscriptionWebHookController extends Controller
                     Mail::to([$user->email, 'anthony.anec@gmail.com'])->send(new EnviarCorreoSuscripcion($user));
                     Log::info('Suscripcion guardada en la bd correctamente');
                 } else {
-                    Log::info('Datos no guardados en la base de datos');
+                    Log::error('Error al guardar la suscripción en la base de datos');
                 }
             } else {
-                Log::info('Usuario no encontrado para el preapproval_id: ' . $preapprovalId);
+                Log::error('Usuario no autenticado para guardar la suscripción');
             }
         } else {
-            Log::info('Datos no recibidos correctamente: ' . json_encode($payloadArray));
+            Log::error('Datos incompletos o incorrectos recibidos del webhook: ' . $payloadRaw);
         }
 
-        // Responder con HTTP 200 para indicar que el webhook fue recibido
+        // Responder con HTTP 200 para indicar que el webhook fue recibido y procesado correctamente
         return response()->json(['status' => 'success'], 200);
     }
 }
