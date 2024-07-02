d4c4a068d750cd845ff1ef990e48964fbe51f19a
Eduardo <picis_9716@hotmail.com>
2024-07-02 12:18:53 -0500

    PLATAFORMA AulaDigital V.1.0

--- app/Http/Controllers/payment/PaymentSuscriptionWebHookController.php
+++ app/Http/Controllers/payment/PaymentSuscriptionWebHookController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\payment;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PaymentSuscriptionWebHookController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $payloadArray = $request->all();
+        $payloadRaw = $request->getContent();
+
+        // Registra los datos en el log para depuración (puedes eliminar esto en producción)
+        Log::info('Webhook Received (Array):', $payloadArray);
+        Log::info('Webhook Received (Raw):', ['payload' => $payloadRaw]);
+    }
 }
