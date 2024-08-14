1f4fb6e04863f8673cdd7bf39ce5ea6d97772c99
Eduardo <picis_9716@hotmail.com>
2024-08-14 14:47:12 -0500

    PLATAFORMA AualaDigital V.1.0

--- resources/views/livewire/course-status.blade.php
+++ resources/views/livewire/course-status.blade.php
@@ -150,8 +150,7 @@
                                     <div class="modal-dialog">
                                         <div class="modal-content">
                                             <div class="modal-header">
-                                                <h1 class="modal-title fs-5" id="MoldalReferenciaVideoLabel">Modal
-                                                    title</h1>
+                                                <h1 class="modal-title fs-5" id="MoldalReferenciaVideoLabel">Refernecia del vídeo</h1>
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
                                             </div>
