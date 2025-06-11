c3950189847ab21fbf4fffb4e9990d57d6662f51
Eduardo <picis_9716@hotmail.com>
2025-06-11 14:15:04 -0500

    PRUEBAS CON TWILIO AGREGADO TABLA WhatsAppsSchedule

--- app/Models/WhatsAppsSchedule.php
+++ app/Models/WhatsAppsSchedule.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class WhatsAppsSchedule extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'phone',
+        'day',
+        'time'
+    ];
 }
