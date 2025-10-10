810c6f00ae268d3a102585950045ed6481b3997e
Eduardo <picis_9716@hotmail.com>
2025-10-10 15:44:10 -0500

    ACTUALIZACION VISTA

--- resources/views/template/nav-admin.blade.php
+++ resources/views/template/nav-admin.blade.php
@@ -88,7 +88,7 @@
                 </ul>
             </nav>
             <label for="btn-menu"><svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-square-x"
-                    width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#ffffff" fill="none"
+                    width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#1a1f71" fill="none"
                     stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                     <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z" />
